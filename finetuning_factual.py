2ff5c6748b9a97ec51de1851041d9d70052b0e23
Pranavkumar Mallela <87595299+pranav-mallela@users.noreply.github.com>
2025-11-22 16:00:30 -0500

    Finetuning code to use the model and dataset from nb

--- finetuning_factual.py
+++ finetuning_factual.py
@@ -1,33 +1,41 @@
-import evaluate
-import random
-import wandb
-import torch
-import pandas as pd
-import json
-from datasets import Dataset, DatasetDict
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-from peft import LoraConfig, get_peft_model
-from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
-from dotenv import load_dotenv
+from transformers import Trainer, TrainingArguments
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from datasets import load_from_disk
 
+# Load model from memory
+model = AutoModelForSequenceClassification.from_pretrained("my_model/")
+tokenizer = AutoTokenizer.from_pretrained("my_model/")
 
-def set_seed(seed):
-    random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
+# Load tokenized dataset from memory
+tokenized_dataset = load_from_disk("tokenized_dataset")
 
-SEED = 42
-set_seed(SEED)
-device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-
-
-load_dotenv() # huggingface and wandb credentials
-
-wandb.login()
-
-run = wandb.init(
-    project='MBert-Factual',
-    job_type="training",
-    anonymous="allow"
+# Define training args
+training_args = TrainingArguments(
+    output_dir= "ModernBERT-domain-classifier",
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=2,
+    learning_rate=5e-5,
+    num_train_epochs=1,
+    bf16=True, # bfloat16 training
+    optim="adamw_torch_fused", # improved optimizer
+    # logging & evaluation strategies
+    logging_strategy="steps",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    save_total_limit=2,
+    load_best_model_at_end=True,
 )
+
+print("Training Args set...")
+
+# Create a Trainer instance
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"]
+)
+
+print("Starting Training...")
+trainer.train()
+print("Training Completed.")
